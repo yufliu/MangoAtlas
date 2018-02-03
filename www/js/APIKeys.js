0862d75b3ad8334c5a3213686896479cfbe489d3
Yu Liu <yuliu1996@yahoo.com>
2018-02-03 13:42:19 -0500

    Add files via upload

---
+++ www/js/APIKeys.js
@@ -0,0 +1,16 @@
+/*
+ * IndoorAtlas Cordova Plugin Examples
+ * https://github.com/IndoorAtlas/cordova-plugin
+ * https://github.com/IndoorAtlas/sdk-cordova-examples
+ */
+
+// API KEYS FOR INDOORATLAS SERVICES AND GOOGLE MAPS JAVASCRIPT API
+
+// Get Google Maps API Key from here: https://console.developers.google.com/apis/credentials
+//var GOOGLE_API_KEY = 'AIzaSyBkZ846iR08RmcdFPPLFRrrxkgWjEjcUMo';
+var GOOGLE_API_KEY = 'AIzaSyDGAC6ToJ7nVUFlFl6oawDVmcWOmeA-yKE';
+// Get IndoorAtlas API Key and Secret from here https://app.indooratlas.com/apps
+var IA_API_KEY = '71668688-a1b2-46f2-b820-29959a8995ee';
+var IA_API_SECRET = '006KEICue6ThISFLF84J2Qd7LA7BaJwdzZrEpam5R/ZF/lqwBcjyejOv85n9srS5iH381x/RqxtXO4RWPQKUeVwOj3Q1gprqJpyiWgCLPA1m2J/WaTjDVRnc9EsP3g==';
+
+//cordova platform add https://github.com/apache/cordova-ios.git#4.4.0-ios-sim
